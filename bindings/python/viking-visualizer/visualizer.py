451cd1e4d945564be7aa4afbf9ce42ec04bc1ae7
Iwan Schafer <35800254+schafei@users.noreply.github.com>
2018-02-23 13:18:39 +0100

    made a displayText function

--- bindings/python/viking-visualizer/visualizer.py
+++ bindings/python/viking-visualizer/visualizer.py
@@ -4,6 +4,21 @@ import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
+def displaytext(text, matrix, font, line, textColor):
+    offscreen_canvas = matrix.CreateFrameCanvas()
+    pos = offscreen_canvas.width
+    
+    while True:
+        offscreen_canvas.Clear()
+        len = graphics.DrawText(offscreen_canvas, font, pos, 2 + line * 12, textColor, text)
+        pos -= 1
+        if (pos + len < 0):
+            pos = offscreen_canvas.width
+
+        time.sleep(0.05)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+
+
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.cols = 64
@@ -17,21 +32,11 @@ matrix = RGBMatrix(options = options)
 
 try:
     print("Press CTRL-C to stop.")
-    offscreen_canvas = matrix.CreateFrameCanvas()
+    
     font = graphics.Font()
-    font.LoadFont("../../../fonts/7x13.bdf")
-    textColor = graphics.Color(255, 255, 0)
-    pos = offscreen_canvas.width
-
-    while True:
-        offscreen_canvas.Clear()
-        len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, "Hello Vikings")
-        pos -= 1
-        if (pos + len < 0):
-            pos = offscreen_canvas.width
-
-        time.sleep(0.05)
-        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+    font.LoadFont("../../../fonts/helvR12.bdf")
+    textColor = graphics.Color(0, 0, 255)
+    displayText("Hello Vikings", matrix, font, 1, textColor)
         
 except KeyboardInterrupt:
     sys.exit(0)
