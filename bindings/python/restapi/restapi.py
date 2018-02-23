d34575a3dab1171fc71daf5f2deb3c2a3835e13b
Iwan Schafer <35800254+schafei@users.noreply.github.com>
2018-02-23 13:59:09 +0100

    trying to combine rest service with displayer

--- bindings/python/restapi/restapi.py
+++ bindings/python/restapi/restapi.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+from viking-visualizer import text-displayer
 
 app = Flask(__name__)
 api = Api(app)
 
 DISPLAY = { 'text': 'Hello Vikings :-)', 'status': 'on' }
+
+textDisplayer = text-displayer()
 
 parser = reqparse.RequestParser()
 parser.add_argument('text')
@@ -20,6 +23,7 @@ class Display(Resource):
         status = args['status']
         if text != None:
             DISPLAY['text'] = text
+            textDisplayer.displayText(text, 1)
         if status != None:
             DISPLAY['status'] = status
         return text, 201
