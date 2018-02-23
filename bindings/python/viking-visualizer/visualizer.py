5f1a39f0ed6c66ad5fc88deec1f49ff46f775862
Iwan Schafer <35800254+schafei@users.noreply.github.com>
2018-02-23 11:36:05 +0100

    first try to display text

--- bindings/python/viking-visualizer/visualizer.py
+++ bindings/python/viking-visualizer/visualizer.py
@@ -3,14 +3,33 @@ import time
 import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
-from PIL import Image
+
+class RunText(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(RunText, self).__init__(*args, **kwargs)
+        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+        textColor = graphics.Color(255, 255, 0)
+        pos = offscreen_canvas.width
+        my_text = self.args.text
+
+        while True:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
 
 if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
-
-image = Image.open(image_file)
+    sys.exit("Require a text argument")
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
@@ -23,14 +42,10 @@ options.hardware_mapping = 'adafruit-hat'
 
 matrix = RGBMatrix(options = options)
 
-# Make image fit our screen.
-image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-
-matrix.SetImage(image.convert('RGB'))
-
 try:
     print("Press CTRL-C to stop.")
-    while True:
-        time.sleep(100)
+    run_text = RunText()
+    if (not run_text.process()):
+        run_text.print_help()
 except KeyboardInterrupt:
     sys.exit(0)
