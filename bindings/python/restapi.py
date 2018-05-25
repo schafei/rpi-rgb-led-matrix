b7d46ac8a64ddccba950c20b925ba4f79d257936
Iwan Schafer <iwan.schafer@gmail.com>
2018-05-25 13:23:03 +0200

    made most json paramaters optional

--- bindings/python/restapi.py
+++ bindings/python/restapi.py
@@ -4,10 +4,13 @@ from flask import Flask, jsonify, request, render_template
 from signal import *
 from contentdisplayer import contentdisplayer
 
-display = { 'imagepath': '', 'text': 'Hello Vikings :-)', 'textcolor': ['255', '255', '255'], 'bgcolor': ['0', '0', '0'], 'scroll': 'true', 'blink': 'false' }
 contentdisplayer = contentdisplayer()
 mainRoute = '/display'
 app = Flask(__name__, static_url_path = mainRoute)
+
+#default: no image, white font on black bg, scrolling, not blinking
+displayDefault = { 'imagepath': '', 'text': '', 'textcolor': ['255', '255', '255'], 'bgcolor': ['0', '0', '0'], 'scroll': 'true', 'blink': 'false' }
+display = displayDefault
 
 @app.route(mainRoute + '/', methods=['GET','POST','PUT','DELETE'])
 def routing():
@@ -16,18 +19,27 @@ def routing():
     jsonify(display)
 
   elif request.method == 'PUT':
-    imagename = request.json.get('imagename')
-    if imagename is not None and ".gif" in imagename:
+	#text has to be set, all others are optional (see displayDefault)
+	display = displayDefault
+	display['text'] = request.json.get('text')
+	
+	#imagepath has priority, when imagepath and imagename are set
+	imagename = request.json.get('imagename')
+	if request.json.get('imagepath') is not None:
+	  display['imagepath'] = request.json.get('imagepath')
+	elif imagename is not None and ".gif" in imagename:
       display['imagepath'] = 'images/' + imagename
-    else:
-      display['imagepath'] = ''
- 
-    display['text'] = request.json.get('text')
-    display['textcolor'] = request.json.get('textcolor')
-    display['bgcolor'] = request.json.get('bgcolor')
-    display['scroll'] = request.json.get('scroll')
-    display['blink'] = request.json.get('blink')
-    print("text: " + display['text'])
+	  
+    if request.json.get('textcolor') is not None:
+	  display['textcolor'] = request.json.get('textcolor')
+    if request.json.get('bgcolor') is not None:
+	  display['bgcolor'] = request.json.get('bgcolor')
+    if request.json.get('scroll') is not None:
+	  display['scroll'] = request.json.get('scroll')
+    if request.json.get('blink') is not None:
+	  display['blink'] = request.json.get('blink')
+    
+	print("text: " + display['text'])
     contentdisplayer.display(display['imagepath'], display['text'], display['textcolor'], display['bgcolor'], display['scroll'], display['blink'])
     return jsonify(display), 201
 
